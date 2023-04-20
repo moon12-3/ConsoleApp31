f6a5ae7fae71caa6ec44976157cab63cf3c9511d
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-04-20 14:06:59 +0900

    초기 커밋.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while(x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if(x%3 == 0)
+                {
+                    Console.WriteLine("__@");
+                } else if(x % 3 == 1)
+                {
+                    Console.WriteLine("__^@");
+                } else 
+                {
+                    Console.WriteLine("^__@");
+                }
+                Thread.Sleep(1000);
+                x++;
+            }
+        }
+    }
+}
